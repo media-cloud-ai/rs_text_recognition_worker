64c742e1baa10d594b4f39d990e8f7145bd99872
Marc-Antoine Arnaud <maarnaud@media-io.com>
2020-07-09 09:51:35 +0200

    redefine worker using MCAI Media Worker SDK

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,45 @@
-use mcai_worker_sdk::job::{Job, JobResult};
-use mcai_worker_sdk::start_worker;
-use mcai_worker_sdk::worker::{Parameter, ParameterType};
-use mcai_worker_sdk::{McaiChannel, MessageError, MessageEvent, Version};
+#[macro_use]
+extern crate serde_derive;
 
-mod message;
-mod region_of_interest;
+use mcai_worker_sdk::{
+  start_worker,
+  job::Job,
+  worker::{Parameter, ParameterType},
+  parameter::container::ParametersContainer,
+  trace,
+  FormatContext,
+  Frame,
+  MessageError,
+  MessageEvent,
+  ProcessResult,
+  Version
+};
+
+use stainless_ffmpeg_sys::{
+  av_get_bits_per_pixel, av_pix_fmt_desc_get,
+  AVPixelFormat,
+};
 
 pub mod built_info {
   include!(concat!(env!("OUT_DIR"), "/built.rs"));
 }
 
-#[derive(Debug)]
-struct TextRecognitionEvent {}
+#[derive(Debug, Serialize)]
+pub struct RecognisedText {
+  pts: u64,
+  text: String,
+}
+
+#[derive(Debug, Default)]
+struct TextRecognitionEvent {
+  language: String,
+}
+
+pub const SOURCE_PATH_PARAMETER: &str = "source_path";
+pub const LANGUAGE_PARAMETER: &str = "language";
+pub const DESTINATION_PATH_PARAMETER: &str = "destination_path";
+pub const SAMPLE_RATE_PARAMETER: &str = "sample_rate";
+pub const REGION_OF_INTEREST_PARAMETER: &str = "region_of_interest";
 
 impl MessageEvent for TextRecognitionEvent {
   fn get_name(&self) -> String {
@@ -24,7 +52,7 @@ impl MessageEvent for TextRecognitionEvent {
 
   fn get_description(&self) -> String {
     r#"This worker applies OCR algorithm on the frame specified as parameter.
-It returns the detected text as a JSON array."#
+It returns the detected text for each requested frame."#
       .to_string()
   }
 
@@ -35,31 +63,31 @@ It returns the detected text as a JSON array."#
   fn get_parameters(&self) -> Vec<Parameter> {
     vec![
       Parameter {
-        identifier: message::SOURCE_PATH_PARAMETER.to_string(),
+        identifier: SOURCE_PATH_PARAMETER.to_string(),
         label: "Source path".to_string(),
         kind: vec![ParameterType::String],
         required: true,
       },
       Parameter {
-        identifier: message::LANGUAGE_PARAMETER.to_string(),
+        identifier: LANGUAGE_PARAMETER.to_string(),
         label: "The language to be detected".to_string(),
         kind: vec![ParameterType::String],
         required: true,
       },
       Parameter {
-        identifier: message::DESTINATION_PATH_PARAMETER.to_string(),
+        identifier: DESTINATION_PATH_PARAMETER.to_string(),
         label: "The OCR result file path".to_string(),
         kind: vec![ParameterType::String],
         required: true,
       },
       Parameter {
-        identifier: message::SAMPLE_RATE_PARAMETER.to_string(),
+        identifier: SAMPLE_RATE_PARAMETER.to_string(),
         label: "The video sampling rate (default: 1)".to_string(),
         kind: vec![ParameterType::Integer],
         required: false,
       },
       Parameter {
-        identifier: message::REGION_OF_INTEREST_PARAMETER.to_string(),
+        identifier: REGION_OF_INTEREST_PARAMETER.to_string(),
         label: "The part of the frame to focus on.".to_string(),
         kind: vec![ParameterType::String], // FIXME should be RegionOfInterest
         required: false,
@@ -67,18 +95,64 @@ It returns the detected text as a JSON array."#
     ]
   }
 
-  fn process(
-    &self,
-    channel: Option<McaiChannel>,
-    job: &Job,
-    job_result: JobResult,
-  ) -> Result<JobResult, MessageError> {
-    message::process(channel, job, job_result)
+  fn init_process(&mut self, job: &Job, _format_context: &FormatContext) -> Result<Vec<usize>, MessageError> {
+    self.language =
+      job.get_parameter("language").map_err(|e| {
+        MessageError::RuntimeError(e.to_string())
+      })?;
+
+    Ok(vec![0])
+  }
+
+  fn process_frame(
+    &mut self,
+    job_id: &str,
+    _stream_index: usize,
+    frame: Frame,
+  ) -> Result<ProcessResult, MessageError> {
+
+    let recognised_text =
+      unsafe {
+        let pixel_format = std::mem::transmute::<_, AVPixelFormat>((*frame.frame).format);
+
+        let av_pix_fmt_desc = av_pix_fmt_desc_get(pixel_format);
+        let bytes_per_pixel = av_get_bits_per_pixel(av_pix_fmt_desc) / 8;
+
+        let width = (*frame.frame).width;
+        let height = (*frame.frame).height;
+        let linesize = (*frame.frame).linesize[0];
+
+        let buffer_size = (linesize * height) as usize;
+
+        let data: Vec<u8> = Vec::from_raw_parts(
+          (*frame.frame).data[0],
+          buffer_size,
+          buffer_size,
+        );
+
+        let text = tesseract::ocr_from_frame(
+          &data,
+          width,
+          height,
+          bytes_per_pixel,
+          linesize,
+          &self.language,
+        ).unwrap();
+        trace!(target: &job_id, "{:?}", text);
+
+        std::mem::forget(data);
+
+        RecognisedText {
+          pts: (*frame.frame).pts as u64,
+          text,
+        }
+      };
+
+    Ok(ProcessResult::new_json(recognised_text))
   }
 }
 
-static TEXT_RECOGNITION_EVENT: TextRecognitionEvent = TextRecognitionEvent {};
-
 fn main() {
-  start_worker(&TEXT_RECOGNITION_EVENT);
+  let worker = TextRecognitionEvent::default();
+  start_worker(worker);
 }
